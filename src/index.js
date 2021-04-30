34c05fcacc204489014ba9583f4be698bed27156
VasylMalion <malionv@ukr.net>
2021-04-30 13:07:45 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const App = () => {
+    return <div>
+        App
+    </div>
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
